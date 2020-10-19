f769fe8cf944d4cb3d50570496579bf608fd54bc
Alby Tseng <albytseng@gmail.com>
2020-10-18 18:34:23 -0700

    Add some wassup

--- test/hello_test.dart
+++ test/hello_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('Hello Test', () {
     test('hey', () {
+      print("What's going on?");
       expect(1 + 1, 2);
     });
   });
